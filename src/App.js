b18c81a74f9f7f4bd7983c974b2341a10e007775
Karamjeet Singh <janduk@student.douglascollege.ca>
2023-03-11 11:39:20 -0800

    worked on login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -7,6 +9,15 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
