59a3e08f28f09e82810a97c1cd4895e687f06f4f
Karamjeet Singh <janduk@student.douglascollege.ca>
2023-03-13 13:30:21 -0700

    worked on leftside of home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,83 @@
 import styled from "styled-components";
+import LeftSide from "./LeftSide";
+import RightSide from "./RightSide";
+import Main from "./Main";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Hiring in a hurry? - </a>
+        </h5>
+        <p>
+          Find talented pros in record time with Upwork and keep business moving
+        </p>
+      </Section>
+      <Layout>
+        <LeftSide />
+        <Main />
+        <RightSide />
+      </Layout>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  padding-top: 52px;
+  max-width: 100%;
+`;
+
+const Section = styled.div`
+  min-height: 50px;
+  padding: 16px 0;
+  box-sizing: border-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+
+  h5 {
+    color: #0a68c2;
+    font-size: 14px;
+
+    a {
+      font-weight: 700;
+    }
+  }
+
+  p {
+    font-size: 14;
+    color: #434649;
+    font-weight: 600;
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Content = styled.div`
+  max-width: 1128px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5ft) minmax(0, 12ft) minmax(300px, 7ft);
+  column-gap: 25px;
+  row-gap: 25px;
+  grid-template-rows: auto;
+  margin: 25px 0;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
 
 export default Home;
---
+++ src/components/RightSide.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const RightSide = () => {
+  return <Container>Right</Container>;
+};
+
+const Container = styled.div`
+  grid-area: rightside;
+`;
+
+export default RightSide;
